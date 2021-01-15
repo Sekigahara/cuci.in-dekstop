f106473b34e957cc43ecb5c2facee6400bb8d6ff
Sekigahara <androaprila12@gmail.com>
2021-01-15 18:51:12 +0700

    none

--- Cuciin-Dekstop/obj/Debug/Cuciin-Dekstop_Content.g.i.cs
+++ Cuciin-Dekstop/obj/Debug/Cuciin-Dekstop_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("historydetail/background_nav.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("historydetail/logo_cuciin.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logo-cuci-in.ico")]
 
 
